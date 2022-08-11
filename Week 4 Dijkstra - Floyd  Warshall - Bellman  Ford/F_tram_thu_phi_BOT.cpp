25d866d2b8099db6cbb0a69ad2ed38cafe1d9d76
PhNam24 <98546157+PhNam24@users.noreply.github.com>
2022-08-12 02:28:46 +0700

    week4

--- Week 4 Dijkstra - Floyd  Warshall - Bellman  Ford/F_tram_thu_phi_BOT.cpp
+++ Week 4 Dijkstra - Floyd  Warshall - Bellman  Ford/F_tram_thu_phi_BOT.cpp
@@ -1,54 +1,54 @@
-#include <bits/stdc++.h>
-#define test int t; cin >> t; while (t--)
-#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    #include <bits/stdc++.h>
+    #define test int t; cin >> t; while (t--)
+    #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-using namespace std;
+    using namespace std;
 
-int n , m , k , s , t;
-vector<pair<int,int>> ke[1005];
-int dist[1005];
-int cnt[1005] = {};
-int ans = 1e9;
+    int n , m , k , s , t;
+    vector<pair<int,int>> ke[1005];
+    int dist[1005];
+    int cnt[1005] = {};
+    int ans = 1e9;
 
-int dijkstra (int u)
-{
-   
-    priority_queue<pair<int,int>> pq;
-    dist[u] = 0;
-    pq.push({0 , u});
-    while (!pq.empty())
+    int dijkstra (int u)
     {
-        pair<int,int> tmp = pq.top();
-        pq.pop();
-        if ( tmp.first <= dist[tmp.second] )
+    
+        priority_queue<pair<int,int>> pq;
+        dist[u] = 0;
+        pq.push({0 , u});
+        while (!pq.empty())
         {
-            for ( auto i : ke[tmp.second] )
+            pair<int,int> tmp = pq.top();
+            pq.pop();
+            if ( tmp.first <= dist[tmp.second] )
             {
-                if ( dist[i.first] > dist[tmp.second] + i.second )
+                for ( auto i : ke[tmp.second] )
                 {
-                    dist[i.first] = dist[tmp.second] + i.second;
-                    cnt[i.first] = cnt[tmp.second] + 1;
-                    pq.push({dist[i.first] , i.first});
-                } 
-                if ( i.first == t && cnt[i.first] <= k ) ans = min(ans, dist[t]);
+                    if ( dist[i.first] > dist[tmp.second] + i.second )
+                    {
+                        dist[i.first] = dist[tmp.second] + i.second;
+                        cnt[i.first] = cnt[tmp.second] + 1;
+                        pq.push({dist[i.first] , i.first});
+                    } 
+                    if ( i.first == t && cnt[i.first] <= k ) ans = min(ans, dist[t]);
+                }
             }
         }
+        if ( ans == 1e9 ) return -1;
+        return ans;
     }
-    if ( ans == 1e9 ) return -1;
-    return ans;
-}
 
-int main ()
-{
-    fast
-    fill(dist , dist + 1005 , 1e9);
-    cin >> n >> m >> k >> s >> t;
-    while (m--)
+    int main ()
     {
-        int x , y , z;
-        cin >> x >> y >> z;
-        ke[x].push_back({y , z});
+        fast
+        fill(dist , dist + 1005 , 1e9);
+        cin >> n >> m >> k >> s >> t;
+        while (m--)
+        {
+            int x , y , z;
+            cin >> x >> y >> z;
+            ke[x].push_back({y , z});
+        }
+        cout << dijkstra(s);
+        return 0;
     }
-    cout << dijkstra(s);
-    return 0;
-}
