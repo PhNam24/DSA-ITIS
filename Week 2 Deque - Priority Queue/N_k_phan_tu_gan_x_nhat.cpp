3341ae23505e8f80f918a653ba4772bb6b3c9189
PhNam24 <98546157+PhNam24@users.noreply.github.com>
2022-08-14 01:46:42 +0700

    change

--- Week 2 Deque - Priority Queue/N_k_phan_tu_gan_x_nhat.cpp
+++ Week 2 Deque - Priority Queue/N_k_phan_tu_gan_x_nhat.cpp
@@ -9,15 +9,32 @@ int main ()
     fast
     int n , k , x;
     cin >> n >> k >> x;
-    int a[n];
+    int a[n] , b[n];
     for ( int i = 0 ; i < n ; i++ )
     {
         cin >> a[i];
+        b[i] = abs(a[i] - x);
     }
-    priority_queue<int , vector<int> , greater<int>> pq;
-    for ( int i = 0 ; i < n ; i++ )
+    priority_queue<pair<int,int>> pq;
+    for ( int i = 0 ; i < k ; i++ )
     {
-        
+        pq.push({b[i], i});
     }
+    for ( int i = k ; i < n ; i++ )
+    {
+        if ( b[i] < pq.top().first )
+        {
+            pq.pop();
+            pq.push({b[i] , i});
+        }
+    }
+    vector<int> ans;
+    while (!pq.empty())
+    {
+        ans.push_back(a[pq.top().second]);
+        pq.pop();
+    }
+    sort(ans.begin() , ans.end());
+    for ( int i : ans ) cout << i << " ";
     return 0;
 }
